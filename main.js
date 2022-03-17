f694c61d3810bfd51235c707706b94087e07c057
Paulina Szybinska <paulina.szybinska@gmail.com>
2022-03-17 21:28:26 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+app.use(expressLayouts)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cookieParser())
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+app.set('view engine', 'ejs')
+app.set('views', `${__dirname}/../views`)
+
+app.listen(3000, () => {
+    console.log(`http://localhost:3000/`)
+})
