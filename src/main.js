8be6b799964b7cc160b9981bf5d37650d9dd763c
Paulina Szybinska <paulina.szybinska@gmail.com>
2022-03-25 09:54:17 +0100

    Views added

--- src/main.js
+++ src/main.js
@@ -17,9 +17,31 @@ app.use(cookieParser());
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
+app.use('./img', express.static(__dirname + 'public/img'));
+
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/../views`);
+
+
+app.get('/', (req, res) => {
+    res.render('login')
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+});
+
+app.get('/new', (req, res) => {
+    res.render('newSchedule');
+});
+
+app.route('/homepage')
+    .get(async (req, res) => {
+        const schedules = await pool.query('SELECT * FROM schedules;');
+        res.render('homepage', {schedules: schedules.rows});
+    })
+    
 
 app.listen(3000, () => {
     console.log(`http://localhost:3000/`)
